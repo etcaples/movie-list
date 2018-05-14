759feea5597352c1c39b90f6c25433967566f963
Emily Caples <etcaples@gmail.com>
2018-05-14 07:10:47 -0700

    renders movie list to page; updates gitignore

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+var App = () => (
+  <div>
+    <ul>
+      <li>Mean Girls</li>
+      <li>Hackers</li>
+      <li>The Grey</li>
+      <li>Sunshine</li>
+      <li>Ex Machina</li>
+    </ul>
+  </div>
+);
+
+window.App = App;
